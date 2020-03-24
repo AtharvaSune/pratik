028072bfa5693af5638077376d2204f17448342d
Atharva Sune <atharvaprakash99@gmail.com>
2020-03-24 20:34:17 +0530

    added supported for input python file

--- python_final_file.py
+++ python_final_file.py
@@ -1,4 +1,5 @@
 # Importing All The Relevant Packages
+import sys
 import numpy as np
 from scipy.integrate import ode
 
@@ -22,30 +23,30 @@ r = ode(newton).set_integrator('dopri5')
 t0 = 0
 
 print('Enter the coordinates of the initial position of the electron :')
-coor_x = input('Enter the x co-ordinate: ')
-coor_y = input('Enter the y co-ordinate: ')
-coor_z = input('Enter the z co-ordinate: ')
+coor_x = sys.argv[1]#input('Enter the x co-ordinate: ')
+coor_y = sys.argv[2]#input('Enter the y co-ordinate: ')
+coor_z = sys.argv[3]#input('Enter the z co-ordinate: ')
 x0 = np.array([int(coor_x),int(coor_y),int(coor_z)])
 
 print('Enter the initial velocity of the electron:')
-vel_x = input('Enter the velocity in x direction: ')
-vel_y = input('Enter the velocity in y direction: ')
-vel_z = input('Enter the velocity in z direction: ')
+vel_x = sys.argv[4]#input('Enter the velocity in x direction: ')
+vel_y = sys.argv[5]#input('Enter the velocity in y direction: ')
+vel_z = sys.argv[6]#input('Enter the velocity in z direction: ')
 v0 = np.array([int(vel_x),int(vel_y),int(vel_z)])
 
 print('Enter the magnitude of magnetic field in space:')
-B_x = input('Enter the magnitude of magnetic field in x direction: ')
-B_y = input('Enter the magnitude of magnetic field in y direction: ')
-B_z = input('Enter the magnitude of magnetic field in z direction: ')
+B_x = sys.argv[7]#input('Enter the magnitude of magnetic field in x direction: ')
+B_y = sys.argv[8]#input('Enter the magnitude of magnetic field in y direction: ')
+B_z = sys.argv[9]#input('Enter the magnitude of magnetic field in z direction: ')
 B0 = np.array([int(B_x),int(B_y),int(B_z)])
 
 print('Enter the magnitude of electric field in space :')
-E_x = input('Enter the magnitude of electric field in x direction: ')
-E_y = input('Enter the magnitude of electric field in y direction: ')
-E_z = input('Enter the magnitude of electric field in z direction: ')
+E_x = sys.argv[10]#input('Enter the magnitude of electric field in x direction: ')
+E_y = sys.argv[11]#input('Enter the magnitude of electric field in y direction: ')
+E_z = sys.argv[12]#input('Enter the magnitude of electric field in z direction: ')
 E0 = np.array([int(E_x),int(E_y),int(E_z)])
 
-Q0 = input('Enter the magnitude of charge: ')
+Q0 = sys.argv[13]#input('Enter the magnitude of charge: ')
 
 #Radius of the helical path formed
 Radius = np.sqrt(v0.dot(v0) - (np.dot(B0,v0)/B0.dot(B0))**2)
@@ -61,18 +62,24 @@ while r.successful() and r.t < t1:
     r.integrate(r.t+dt)
     positions.append(r.y[:3]) # keeping only position, not velocity
 
-positions = np.array(positions)
 
-# Intergrating The Results
-import matplotlib.pyplot as plt
+with open("file_io_positions0.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow([0, 1, 2])
+    writer.writerows(positions)
+    del writer
+# positions = np.array(positions)
 
-import matplotlib as mpl
-from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.plot3D(positions[:, 0], positions[:, 1], positions[:, 2])
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-ax.set_zlabel('z-axis')
-mpl.pyplot.title('Flow of Electron', loc='left')
-plt.show()
+# # Intergrating The Results
+# import matplotlib.pyplot as plt
+
+# import matplotlib as mpl
+# from mpl_toolkits.mplot3d import Axes3D
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot3D(positions[:, 0], positions[:, 1], positions[:, 2])
+# plt.xlabel('x-axis')
+# plt.ylabel('y-axis')
+# ax.set_zlabel('z-axis')
+# mpl.pyplot.title('Flow of Electron', loc='left')
+# plt.show()
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const csv = require("csv-parser");
-const fs = require("fs")
+const fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
   var spawn = require("child_process").spawn;
   var process = spawn('python', ["../Final_Python_File.py"]); // py file => Final_Python_File.py
-  var positions0=[];
-  var positions1 = []
+  var positions0=[]; // variable to plot
+  var positions1 = []; // variable to plot
   process.on("close", (err)=>{
     fs.createReadStream("../file_no_io_positions0.csv")
     .pipe(csv())
     .on('data', (row)=>{
-      var temp = []
-      temp.push(parseFloat(row['0']))
-      temp.push(parseFloat(row['1']))
-      temp.push(parseFloat(row['2']))
-      positions0.push(temp)
+      var temp = [];
+      temp.push(parseFloat(row['0']));
+      temp.push(parseFloat(row['1']));
+      temp.push(parseFloat(row['2']));
+      positions0.push(temp);
     })
     fs.createReadStream("../file_no_io_positions1.csv")
     .pipe(csv())
     .on('data', (row)=>{
-      var temp = []
-      temp.push(parseFloat(row['0']))
-      temp.push(parseFloat(row['1']))
-      temp.push(parseFloat(row['2']))
-      positions1.push(temp)
+      var temp = [];
+      temp.push(parseFloat(row['0']));
+      temp.push(parseFloat(row['1']));
+      temp.push(parseFloat(row['2']));
+      positions1.push(temp);
     })
   })
   /**
@@ -37,27 +37,32 @@ router.get('/', function(req, res, next) {
 
 router.post('/', (req, res)=>{
   var spawn = require("child_process").spawn;
-  var process = spawn('python', "../python_final_file.py"); // py file => python_final_file.py
-  var positions0=[];
-  var positions1 = []
+  var process = spawn('python', 
+                  [ "../python_final_file.py", 
+                    req.query.x, 
+                    req.query.y,
+                    req.query.z,
+                    req.query.vx,
+                    req.query.vy,
+                    req.query.vz,
+                    req.query.bx,
+                    req.query.by,
+                    req.query.bz,
+                    req.query.ex,
+                    req.query.ey,
+                    req.query.ez,
+                    req.query.q
+                  ]); // py file => python_final_file.py which requires inputs 13 inputs for info on inputs view the file
+  var positions=[];
   process.on("close", (err)=>{
-    fs.createReadStream("../file_no_io_positions0.csv")
+    fs.createReadStream("../file_io_positions.csv")
     .pipe(csv())
     .on('data', (row)=>{
       var temp = []
-      temp.push(parseFloat(row['0']))
-      temp.push(parseFloat(row['1']))
-      temp.push(parseFloat(row['2']))
-      positions0.push(temp)
-    })
-    fs.createReadStream("../file_no_io_positions1.csv")
-    .pipe(csv())
-    .on('data', (row)=>{
-      var temp = []
-      temp.push(parseFloat(row['0']))
-      temp.push(parseFloat(row['1']))
-      temp.push(parseFloat(row['2']))
-      positions1.push(temp)
+      temp.push(parseFloat(row['0']));
+      temp.push(parseFloat(row['1']));
+      temp.push(parseFloat(row['2']));
+      positions.push(temp);
     })
   })
 })
