1c15ace6e40bca869d5af4db04cdb04f05395721
Ameya Salankar <salankaras1234@gmail.com>
2020-03-25 15:51:54 +0530

    implemented static graph

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,30 @@ const fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
   var spawn = require("child_process").spawn;
   var process = spawn('python', ["./Final_Python_File.py"]); // py file => Final_Python_File.py
-  var positions0=[]; // variable to plot
-  var positions1 = []; // variable to plot
+  var positions0 = []; // variable to plot
+  var positions1 = [];
+
+  var positions0x = []; // variable to plot
+  var positions0y = [];
+  var positions0z = [];
+  var positions1x = []; // variable to plot
+  var positions1y = [];
+  var positions1z = [];
+
   process.on("close", (err)=>{
-    fs.createReadStream("../file_no_io_positions0.csv")
+    fs.createReadStream("./file_no_io_positions0.csv")
     .pipe(csv())
     .on('data', (row)=>{
       var temp = [];
       temp.push(parseFloat(row['0']));
       temp.push(parseFloat(row['1']));
       temp.push(parseFloat(row['2']));
+      positions0x.push(parseFloat(row['0']));
+      positions0y.push(parseFloat(row['1']));
+      positions0z.push(parseFloat(row['2']));
+
       positions0.push(temp);
     })
     .on('end', (err)=>{
@@ -25,13 +36,17 @@ router.get('/', function(req, res, next) {
         console.log(err);
         return;
       }
-        fs.createReadStream("../file_no_io_positions1.csv")
+      fs.createReadStream("./file_no_io_positions1.csv")
       .pipe(csv())
       .on('data', (row)=>{
         var temp = [];
         temp.push(parseFloat(row['0']));
         temp.push(parseFloat(row['1']));
         temp.push(parseFloat(row['2']));
+        positions1x.push(parseFloat(row['0']));
+        positions1y.push(parseFloat(row['1']));
+        positions1z.push(parseFloat(row['2']));
+
         positions1.push(temp);
       })
       .on('end', (err)=>{
@@ -39,11 +54,7 @@ router.get('/', function(req, res, next) {
           console.log(err);
           return;
         }
-        console.log("positions0", positions0, "\n");
-        /**
-        * Code to visualize the plot using the csv files
-        */
-       console.log("positions1", positions1, "\n");
+        res.render('index', { title: 'Express', x0: positions0x, y0: positions0y, z0: positions0z, x1: positions1x, y1: positions1y, z1: positions1z });
       })
     })
   });
@@ -52,7 +63,7 @@ router.get('/', function(req, res, next) {
 router.post('/', (req, res)=>{
   var spawn = require("child_process").spawn;
   var process = spawn('python', 
-                  [ "../python_final_file.py", 
+                  [ "./python_final_file.py", 
                     req.query.x, 
                     req.query.y,
                     req.query.z,
